10e8745c273e52ca73a8fe5cab00b081fc1af758
James Jarvis <jj@aprsworld.com>
2014-09-17 07:11:38 -0500

    New files.

---
+++ src/dataGS/LogNull.java
@@ -0,0 +1,31 @@
+package dataGS;
+
+public class LogNull implements Log {
+
+	public void connect() throws Exception {
+		
+	}
+
+	public void close() {
+		
+	}
+	
+	public void queryAutoCreate(String s, String proto, String table) {
+		
+	}
+	
+
+	public void queryAutoCreateFromString(String s, String sqlToCreate, String table) {
+		
+	}
+	
+	public void insertDigiAPI(String table, String proto, int port, int source, int rssi, int options, String data) {
+		
+	}
+	
+	public void query(String s) {
+		
+	}
+	
+
+}
