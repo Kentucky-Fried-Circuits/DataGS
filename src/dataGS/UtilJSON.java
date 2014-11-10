d800efb60d203c1b3a87206772b39114c349cdc2
James Jarvis <jj@aprsworld.com>
2014-11-10 08:46:56 -0600

    More more more

--- src/dataGS/UtilJSON.java
+++ src/dataGS/UtilJSON.java
@@ -1,6 +1,12 @@
 package dataGS;
 
 public class UtilJSON {
+	/**
+	 * Make a JSON object with a double. Convert a NaN to "null".
+	 * @param object name
+	 * @param double value
+	 * @return string with JSON representation 
+	 */
 	public static String putDouble(String title, Double value) {
 		if ( Double.isNaN(value) ) {
 			return "\"" + title + "\": null";	
@@ -8,6 +14,22 @@ public class UtilJSON {
 		return "\"" + title + "\":" + value; 		
 	}
 	
+	/**
+	 * Make a JSON object with an integer value.
+	 * @param object name
+	 * @param integer value
+	 * @return string with JSON representation 
+	 */
+	public static String putInt(String title, int value) {
+		return "\"" + title + "\": \"" + value + "\"";
+	}
+	
+	/**
+	 * Make a JSON object with an string value.
+	 * @param object name
+	 * @param string value
+	 * @return string with JSON representation 
+	 */
 	public static String putString(String title, String value) {
 		return "\"" + title + "\": \"" + value + "\""; 		
 	}
