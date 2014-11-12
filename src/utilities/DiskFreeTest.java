a0fe405c5f22db3b83b61f50dadf962a69e95b2d
James Jarvis <jj@aprsworld.com>
2014-11-12 08:28:17 -0600

    Trying to find mounted drives under unix

--- src/utilities/DiskFreeTest.java
+++ src/utilities/DiskFreeTest.java
@@ -8,34 +8,46 @@ import java.text.NumberFormat;
 public class DiskFreeTest {
 	public static void main(String args[]) {
 		NumberFormat nf = NumberFormat.getNumberInstance();
-		
-//		Path root = FileSystems.getDefault().getPath("www/foo.bar");
 
-		
-		
-		
+		//		Path root = FileSystems.getDefault().getPath("www/foo.bar");
+
+
+		for (FileStore store : FileSystems.getDefault().getFileStores()) {
+			try { 
+				long total = store.getTotalSpace() / 1024;
+				long used = (store.getTotalSpace() - store.getUnallocatedSpace()) / 1024;
+				long avail = store.getUsableSpace() / 1024;
+				System.out.format("%-20s %12d %12d %12d%n", store, total, used, avail);
+			} catch ( Exception e ) {
+				e.printStackTrace();
+			}
+		}
+
+
+
+
 		//for (Path root : FileSystems.getDefault().getRootDirectories()) {
 		for ( int i=0 ; i<args.length ; i++ ) {
 			Path root = FileSystems.getDefault().getPath(args[i]);
-			
+
 			System.out.print(root + " ");
 
 			try	{
 				FileStore store = Files.getFileStore(root);
-				
+
 				long available = store.getUsableSpace();
 				long total = store.getTotalSpace();
-				
+
 				long availableMegabytes = available / 1024;
 				long totalMegabytes = total / 1024;
 				double percentFree = ( (double) availableMegabytes / (double) totalMegabytes) * 100.0;
-				
+
 				System.out.println("readyOnly=" + store.isReadOnly() + " available=" + availableMegabytes + " total=" + totalMegabytes + " % free=" + percentFree );
-				
-			
-				
-				
-//				System.out.println("available=" + nf.format(store.getUsableSpace()) + ", total=" + nf.format(store.getTotalSpace()));
+
+
+
+
+				//				System.out.println("available=" + nf.format(store.getUsableSpace()) + ", total=" + nf.format(store.getTotalSpace()));
 			} catch (Exception e)	{
 				System.out.println("error querying space: " + e.toString());
 			}
